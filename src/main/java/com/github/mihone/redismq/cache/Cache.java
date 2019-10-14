e4018a06d35725ee3255648591368ec06e8d1f34
mihone <xros1178217769@gmail.com>
2019-10-14 20:58:16 +0800

    del unused file

--- src/main/java/com/github/mihone/redismq/cache/Cache.java
+++ src/main/java/com/github/mihone/redismq/cache/Cache.java
@@ -4,12 +4,24 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
 
 public class Cache {
     private static final ConcurrentHashMap<String, Method> METHOD_CACHE = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Future, String> QUEUE_LISTENER_CACHE = new ConcurrentHashMap<>();
 
     public static void writeToMethodCache(String key, Method value) {
         METHOD_CACHE.put(key, value);
+    }
+
+    public static String getFromQueueListenerCache(Future key) {
+        return QUEUE_LISTENER_CACHE.get(key);
+    }
+    public static void writeToQueueListenerCache(Future key, String value) {
+        QUEUE_LISTENER_CACHE.put(key, value);
+    }
+    public static void removeFromQueueListenerCache(Future key) {
+        QUEUE_LISTENER_CACHE.remove(key);
     }
 
     public static Method getFromMethodCache(String key) {
